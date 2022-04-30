f23862a7709e222120c10756b0ed2aa67ece0c87
ryu <ryu000407@gmail.com>
2022-05-01 04:15:45 +0900

    update: prettier config

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,13 +8,13 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--double-quote", "--jsx-single-quote" } }),
 		formatting.stylua,
 		formatting.rustfmt,
 		formatting.goimports,
 		formatting.golines,
 		formatting.terraform_fmt,
-        formatting.sqlformat,
+		formatting.sqlformat,
 	},
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
